eef26271f0ef9014ea98792f71c8513f2ef61cde
hugarili <rafi11m@hotmail.com>
2020-08-05 19:31:07 +0200

    added Schedule create and delete folders

--- src/main/java/org/sharefiles/root/helpers/OwnDateFormatter.java
+++ src/main/java/org/sharefiles/root/helpers/OwnDateFormatter.java
@@ -1,18 +1,28 @@
 package org.sharefiles.root.helpers;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.Date;
 
 public class OwnDateFormatter {
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 
     public static String getSimpleDateFormat(){
         return dateFormat.format(new Date());
     }
 
     public static String getNextDayDate(){
-        return null;
+        Calendar tomorrowDate = Calendar.getInstance();
+        tomorrowDate.add(Calendar.DAY_OF_MONTH, 1);
+        return dateFormat.format(tomorrowDate.getTime());
+    }
+
+    public static String getFolderNameToDelete(){
+        Calendar tomorrowDate = Calendar.getInstance();
+        tomorrowDate.add(Calendar.DAY_OF_MONTH, -7);
+        return dateFormat.format(tomorrowDate.getTime());
     }
 
 
--- src/main/java/org/sharefiles/root/helpers/ScheduledTasks.java
+++ src/main/java/org/sharefiles/root/helpers/ScheduledTasks.java
@@ -6,17 +6,48 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
+
+
 @Service
 public class ScheduledTasks {
 
+
     private static Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+    private String todayFolderDirectory = OwnDateFormatter.getSimpleDateFormat();
 
     @Value("${upload.service.main.directory}")
     private String uploadDirectory;
 
-    @Scheduled(cron = "", zone = "Europe/Warsaw")
+    @Scheduled(cron = "0 0 1 * * ?", zone = "Europe/Warsaw")
+    //@Scheduled(fixedRate = 8000)
     public void createDirectoryForCurrentDay(){
-        String currentDayFolderName = OwnDateFormatter.getSimpleDateFormat();
+
+        String tomorrowFolderDirectory = uploadDirectory + OwnDateFormatter.getNextDayDate();
+        try {
+            if(!new File(todayFolderDirectory).exists())
+                Files.createDirectory(Paths.get(todayFolderDirectory));
+            if(!new File(tomorrowFolderDirectory).exists())
+                Files.createDirectory(Paths.get(tomorrowFolderDirectory));
+        } catch (IOException e) {
+            logger.error("There has been an error " + e.getMessage());
+        }
+    }
+
+    @Scheduled(cron = "0 0 1 * * ?", zone="Europe/Warsaw")
+    //@Scheduled(fixedRate= 8000)
+    public void deleteOldFolders(){
+        String folderToDelete = uploadDirectory + OwnDateFormatter.getFolderNameToDelete();
+        try{
+            FileUtils.deleteDirectory(new File(folderToDelete));
+        } catch (IOException e){
+            logger.error("There has been an error " + e.getMessage());
+        }
 
     }
 }
